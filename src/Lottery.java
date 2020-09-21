fb0767b62ce5ff3a48e9e03d9fb081089c3d5a06
nekto007 <nekto.korobko@gmail.com>
2020-09-21 22:15:04 +0300

    Добавлены класс: Lottery

---
+++ src/Lottery.java
@@ -0,0 +1,17 @@
+public class Lottery {
+    public static void main(String[] args) {
+        int[] nums = new int[50];
+        for (int i = 0; i < 50; i++) {
+            nums[i] = i;
+        }
+        for (int i = 0; i < 50; i++) {
+            int r = (int) Math.ceil(Math.random() * 49);
+            int temp = nums[i];
+            nums[i] = nums[r];
+            nums[r] = temp;
+        }
+        for (int i = 0; i < 7; i++) {
+            System.out.println(nums[i] + " ");
+        }
+    }
+}
